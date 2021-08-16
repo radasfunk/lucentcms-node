eff8fa6761340cbc16d0bef93bd69a67172b558a
Aryan Ahmed Anik <thearyanahmed@gmail.com>
2021-08-16 11:54:09 +0600

    using import instead of require

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
-export * from "./lucent_client"
-export * from "./query_builder"
+import LucentQueryBuilder from './query_builder'
+import LucentClient from './lucent_client'
+
+export {
+    LucentClient,
+    LucentQueryBuilder 
+}
