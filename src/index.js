f95a34594bf85fcdcb4e7097a54fbb1bb184cda8
Aryan Ahmed Anik <thearyanahmed@gmail.com>
2021-10-04 21:45:45 +0600

    WIP 1.0.8

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-// import LucentQueryBuilder from './query_builder'
-// import LucentClient from './lucent_client'
+import LucentQueryBuilder from './query_builder'
+import LucentClient from './lucent_client'
 
-const LucentClient = require('./lucent_client')
-const LucentQueryBuilder = require('./query_builder')
+// const LucentClient = require('./lucent_client')
+// const LucentQueryBuilder = require('./query_builder')
 
-module.exports.LucentClient = LucentClient
-module.exports.LucentQueryBuilder = LucentQueryBuilder
+export {
+    LucentClient,
+    LucentQueryBuilder
+}
